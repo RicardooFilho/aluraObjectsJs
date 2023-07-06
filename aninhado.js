b837b7ad7db85765ec25e2b1b95ff813081f03d3
RicardooFilho <ricardo.tagami17@gmail.com>
2023-07-05 23:06:35 -0300

    Commitando arquivo do tilte

---
+++ aninhado.js
@@ -0,0 +1,15 @@
+const cliente = {
+  nome: "Joao",
+  idade: 24,
+  email: "joao@firma.com",
+  telefone: [1123356677, 897542233],
+};
+
+cliente.endereco = {
+  rua: "R. Joseph Climber",
+  numero: 1337,
+  apartamento: true,
+  complemento: "apto 934"
+}
+
+console.log(cliente.endereco);
