0f88461517552d08399ef15885eb5e37455c5144
RicardooFilho <ricardo.tagami17@gmail.com>
2023-07-08 21:11:26 -0300

    Criando um arquivo JSON e realizando a leitura do mesmo em um arquivo JS

---
+++ leitura.js
@@ -0,0 +1,6 @@
+//Função require() permite transformar o arquivo JSON em um objeto usável em qualquer outro lugar
+const dados = require("./cliente.json");
+
+console.log(dados);
+console.log(`\n`);
+console.log(typeof dados);
